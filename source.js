64102ea927bae3b3586a4355fc4630580c380acf
Karan Patel <kpatel5797@gmail.com>
2018-05-07 01:41:57 -0400

    minor updates

--- source.js
+++ source.js
@@ -1,7 +1,7 @@
 var canvas;
 var canvasContext;
-var ballX = 50;
-var ballY = 50;
+var ballX = 0;
+var ballY = 250;
 var ballSpeedX = 10;
 var ballSpeedY = 4;
 
@@ -11,9 +11,9 @@ const paddle_thick = 10;
 const paddle_height = 100;
 var play1Score = 0;
 var play2Score = 0;
-const win_score = 3;
+const win_score = 5;
 
-var showWinScreen = false;
+var showWinScreen = true;
 
 function calculateMousePos(event) {
 	var rect = canvas.getBoundingClientRect();
@@ -60,12 +60,30 @@ function ballReset() {
 	ballY = canvas.height/2;
 }
 
+function ballReset1() {
+	if(play1Score >= win_score || play2Score >= win_score){
+		showWinScreen = true;
+	}
+	ballSpeedY = 4;
+	ballX = 0;
+	ballY = paddle1Y + paddle_height/2;
+}
+
+function ballReset2() {
+	if(play1Score >= win_score || play2Score >= win_score){
+		showWinScreen = true;
+	}
+	ballSpeedY = -4
+	ballX = canvas.width;
+	ballY = paddle2Y + paddle_height/2;
+}
+
 function compMove(){
 	var p2center = paddle2Y + (paddle_height/2);
 	if(p2center < ballY - 35)
-		paddle2Y += 6;
-	else if(p2center > ballY +35)
-		paddle2Y -= 6;
+		paddle2Y += 9;
+	else if(p2center > ballY + 35)
+		paddle2Y -= 9;
 }
 
 function move(){
@@ -82,8 +100,8 @@ function move(){
 			ballSpeedY = diffY * 0.35;
 		}
 		else {
-			play2Score++; // should be updated brfore reset
-			ballReset();
+			play2Score++; // should be updated before reset
+			ballReset2();
 		}
 	}
 	if(ballX > canvas.width){
@@ -93,8 +111,8 @@ function move(){
 			ballSpeedY = diffY * 0.35;
 		}
 		else {
-			play1Score++; // should be updated brfore reset
-			ballReset();
+			play1Score++; // should be updated before reset
+			ballReset1();
 		}
 	}
 
@@ -110,27 +128,31 @@ function drawNet(){
 	}
 }
 function draw(){
-	colourRect(0, 0, canvas.width, canvas.height, 'green');
+	colourRect(0,0, canvas.width, canvas.height, 'green');
+    canvasContext.font="40px Calibri"
 	canvasContext.fillStyle = 'red';
 	canvasContext.fillText(play1Score, 100, 100);
 	canvasContext.fillText(play2Score, canvas.width - 100, 100);
+	canvasContext.font="25px Calibri"
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillText("PING PONG", 345, 40)
+    canvasContext.font="20px Calibri"
 	if(showWinScreen){
 		canvasContext.fillStyle = 'white';
 		if(play1Score >= win_score){
-			canvasContext.fillText("Congratulations!", 365, 250)
-			canvasContext.fillText("You won the game!!!", 360, 300)
+			canvasContext.fillText("Congratulations!", 340, 250)
+			canvasContext.fillText("You won the game!!!", 320, 300)
 		}
 		else if(play2Score >= win_score){
-			canvasContext.fillText("Better luck next time!", 360, 250)
-			canvasContext.fillText("Computer won the game!!!", 350, 300)
+			canvasContext.fillText("Better luck next time!", 320, 250)
+			canvasContext.fillText("Computer won the game!!!", 300, 300)
 		}
-		canvasContext.fillText("Click anywhere to restart!", 350, 500)
+		canvasContext.fillText("Click anywhere to start the game", 280, 500)
 		return;
 	}
 	
-	canvasContext.fillStyle = 'white';
 	drawNet();
-	colourRect(0, paddle1Y, 10, paddle_height, 'white'); // right paddle
+	colourRect(0, paddle1Y, paddle_thick, paddle_height, 'white'); // right paddle
 	colourRect(canvas.width - paddle_thick, paddle2Y, paddle_thick, paddle_height, 'white'); // left paddle
 	colourCircle(ballX, ballY, 10, 'red');
 	//canvasContext.fillText(play1Score, 100, 100);
